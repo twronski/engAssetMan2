701e15e91fe518818d0b1a14b32c23d315988eda
Thiago Wronski <wronski79@gmail.com>
2019-05-07 01:26:21 -0300

    create tool to convert CSV database to array of hashes - update conversion file

--- csv_to_hash.rb
+++ csv_to_hash.rb
@@ -1,9 +1,4 @@
 require 'csv'
-# csv_data = CSV.read '/home/thiagowronski/code/Rails/engAssetMan2/Ativos_database_rev00.csv'
-# headers = csv_data.shift.map {|i| i.to_s }
-# string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
-# array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
-# puts array_of_hashes
 
 CSV::Converters[:blank_to_nil] = lambda do |field|
     field && field.empty? ? nil : field
