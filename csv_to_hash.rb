699ad79a9763cfe02fd434c582bdce9151a204bd
Thiago Wronski <wronski79@gmail.com>
2019-05-11 03:07:47 -0300

    Update database as hashes and csv_to_hash script

--- csv_to_hash.rb
+++ csv_to_hash.rb
@@ -7,5 +7,5 @@ array_of_hashes = []
 csv_data = File.read('/home/thiagowronski/code/Rails/engAssetMan2/Ativos_database_rev00.csv')
 csv = CSV.new(csv_data, :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
 csv.to_a.map {|row| array_of_hashes << row.to_hash }
-puts array_of_hashes
-puts array_of_hashes.class
+p array_of_hashes
+# puts array_of_hashes.class
