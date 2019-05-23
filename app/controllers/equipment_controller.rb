a4b7daedf8e3c58d9c7d273803a386f8c333625e
Thiago Wronski <wronski79@gmail.com>
2019-05-23 19:43:35 -0300

    Some layout and view implementations

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,5 +1,6 @@
 class EquipmentController < ApplicationController
   def index
+    @equipment = Equipment.all
   end
 
   def new
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,8 +1,10 @@
 class Equipment < ApplicationRecord
   include Friendlyable
+  
   belongs_to :eqpt_type
   belongs_to :eqpt_model
   belongs_to :eqpt_function
+
   validates_presence_of :qty_total, on: :create, message: "can't be blank"
   validates_presence_of :qty_available_for_borrow, on: :create, message: "can't be blank"  
   validates_presence_of :lab_location, on: :create, message: "can't be blank"
